16b18fb64141309ba2e2e7a5908149b9a5dc4256
john <346660561@qq.com>
2020-06-10 11:58:54 +0800

    Update Divide.java

--- src/main/java/com/tree/form/api/calculate/Divide.java
+++ src/main/java/com/tree/form/api/calculate/Divide.java
@@ -1,18 +1,17 @@
-package com.yunsom.form.api.calculate;
+package com.tree.form.api.calculate;
 
-import static com.yunsom.form.api.constant.FunctionEnum.DIVIDE;
-import static com.yunsom.form.api.constant.ResultCodeEnum.DEFINE_FORM_EXPRE_FUNC_INVALID_PARAM;
+import static com.tree.form.api.constant.FunctionEnum.DIVIDE;
+import static com.tree.form.api.constant.ResultCodeEnum.DEFINE_FORM_EXPRE_FUNC_INVALID_PARAM;
 import static java.math.BigDecimal.ROUND_HALF_UP;
 
-import com.yunsom.common.base.exception.BusinessException;
-import com.yunsom.form.api.constant.FunctionEnum;
-import com.yunsom.form.api.constant.ResultCodeEnum;
-import com.yunsom.form.api.util.CollectionUtils;
+import com.tree.common.base.exception.BusinessException;
+import com.tree.form.api.constant.FunctionEnum;
+import com.tree.form.api.constant.ResultCodeEnum;
+import com.tree.form.api.util.CollectionUtils;
 import java.math.BigDecimal;
 import java.util.List;
 
 /**
- * @author zhuzhong@yunsom.com
  * @date 2020-06-09 15:53
  * @description
  */
