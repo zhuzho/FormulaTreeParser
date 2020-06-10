0e94d787131ed57799cd40f57f833d202c66cd8b
john <346660561@qq.com>
2020-06-10 11:56:21 +0800

    Update FeignConfiguration.java

--- src/main/java/com/tree/form/api/FeignConfiguration.java
+++ src/main/java/com/tree/form/api/FeignConfiguration.java
@@ -1,12 +1,1 @@
-package com.yunsom.form.api;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients
-@ComponentScan({"com.yunsom.form.api.phpapi"})
-public class FeignConfiguration {
-
-}
